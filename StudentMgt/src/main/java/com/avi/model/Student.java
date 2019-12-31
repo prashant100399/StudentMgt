4b8af055872c738a7cb8af1f90618f1a274875d1
prashant100399 <prashantavipatil@gmail.com>
2019-12-31 21:05:20 +0530

    Adding setter getter method with parameter constructor also no-args constructor

--- StudentMgt/src/main/java/com/avi/model/Student.java
+++ StudentMgt/src/main/java/com/avi/model/Student.java
@@ -1,5 +1,46 @@
 package com.avi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
 public class Student {
+	
+	private int rollno;
+	private String name;
+	private String address;
+	
+	public Student() {
+		super();
+	}
+
+	
+	public Student(int rollno, String name, String address) {
+		super();
+		this.rollno = rollno;
+		this.name = name;
+		this.address = address;
+	}
+	
+	
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 }
